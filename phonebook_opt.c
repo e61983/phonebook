8323c140e11fed45c7de8284bb3a5274854232dd
Hua-Yuan <e61983+git@gmail.com>
2016-02-26 22:49:13 +0800

    origin version find function

--- phonebook_opt.c
+++ phonebook_opt.c
@@ -6,7 +6,11 @@
 /* FILL YOUR OWN IMPLEMENTATION HERE! */
 entry *findName(char lastname[], entry *pHead)
 {
-    /* TODO: implement */
+    while (pHead != NULL) {
+        if (strcasecmp(lastname, pHead->lastName) == 0)
+            return pHead;
+        pHead = pHead->pNext;
+    }
     return NULL;
 }
 
