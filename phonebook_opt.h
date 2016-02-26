e707f3b917d734bef3d13759ee28b72caa1c3329
Hua-Yuan <e61983+git@gmail.com>
2016-02-26 22:48:30 +0800

    move detail data to ohter datastruct

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -5,9 +5,7 @@
 
 /* TODO: After modifying the original version, uncomment the following
  * line to set OPT properly */
-// #define OPT 1
-typedef struct __PHONE_BOOK_ENTRY {
-    char lastName[MAX_LAST_NAME_SIZE];
+typedef struct detail{
     char firstName[16];
     char email[16];
     char phone[10];
@@ -17,8 +15,14 @@ typedef struct __PHONE_BOOK_ENTRY {
     char city[16];
     char state[2];
     char zip[5];
-    struct __PHONE_BOOK_ENTRY *pNext;
-} entry;
+} detail, *p_detail;
+
+typedef struct entry {
+    char lastName[MAX_LAST_NAME_SIZE];
+    p_detail detailInfo;
+    struct entry *pNext;
+} entry, *p_entry;
+
 
 entry *findName(char lastname[], entry *pHead);
 entry *append(char lastName[], entry *e);
